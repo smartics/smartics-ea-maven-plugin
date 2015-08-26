df8172ac175eafdea4e7b2f2263e35b9af57316c
Robert Reiner <robert.reiner@smartics.de>
2015-08-26 16:00:45 +0200

    Updated maven-plugin-plugin to version 3.4
    Fix Javadoc problems with with Java 8. Reactivated help mojo.

--- src/main/java/de/smartics/maven/ea/XmiPackageExport.java
+++ src/main/java/de/smartics/maven/ea/XmiPackageExport.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2014-2015 smartics, Kronseder & Reiner GmbH
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package de.smartics.maven.ea;
 
@@ -19,13 +19,13 @@ import org.sparx.EnumXMIType;
 
 /**
  * Stores the configuration the the XMI package export.
- *
- * @see http
- *      ://www.sparxsystems.com/enterprise_architect_user_guide/9.3/automation
- *      /project_2.html
+ * <p>
+ * For details please refer to <a href=
+ * "http://www.sparxsystems.com/enterprise_architect_user_guide/9.3/automation/project_2.html">
+ * User Guide</a>.
+ * </p>
  */
-public class XmiPackageExport
-{
+public class XmiPackageExport {
   // ********************************* Fields *********************************
 
   // --- constants ------------------------------------------------------------
@@ -53,9 +53,7 @@ public class XmiPackageExport
   /**
    * Default constructor.
    */
-  public XmiPackageExport()
-  {
-  }
+  public XmiPackageExport() {}
 
   // ****************************** Inner Classes *****************************
 
@@ -63,15 +61,12 @@ public class XmiPackageExport
 
   // --- init -----------------------------------------------------------------
 
-  public void init() throws IllegalArgumentException
-  {
+  public void init() throws IllegalArgumentException {
     this.xmiType = asXmiType();
   }
 
-  private EnumXMIType asXmiType() throws IllegalArgumentException
-  {
-    if (this.xmiTypeId == null)
-    {
+  private EnumXMIType asXmiType() throws IllegalArgumentException {
+    if (this.xmiTypeId == null) {
       return EnumXMIType.xmiEADefault;
     }
     final EnumXMIType xmiType = EnumXMIType.valueOf(this.xmiTypeId);
@@ -82,43 +77,36 @@ public class XmiPackageExport
 
   // --- business -------------------------------------------------------------
 
-  public EnumXMIType getXmiType()
-  {
+  public EnumXMIType getXmiType() {
     return xmiType;
   }
 
-  public int getDiagramXml()
-  {
+  public int getDiagramXml() {
     return diagramXml;
   }
 
-  public int getDiagramImage()
-  {
+  public int getDiagramImage() {
     return diagramImage;
   }
 
-  public int getFormatXml()
-  {
+  public int getFormatXml() {
     return formatXml;
   }
 
-  public int getUseDtd()
-  {
+  public int getUseDtd() {
     return useDtd;
   }
 
-  public int getXmiFlags()
-  {
+  public int getXmiFlags() {
     return xmiFlags;
   }
 
   // --- object basics --------------------------------------------------------
 
   @Override
-  public String toString()
-  {
+  public String toString() {
     return "xmiType=" + xmiTypeId + ", diagramXml=" + diagramXml
-           + ", diagramImage=" + diagramImage + ", formatXml=" + formatXml
-           + ", useDtd=" + useDtd + ", xmiFlags=" + xmiFlags;
+        + ", diagramImage=" + diagramImage + ", formatXml=" + formatXml
+        + ", useDtd=" + useDtd + ", xmiFlags=" + xmiFlags;
   }
 }
